9b4e5be1c73cda33d66cc834a5d390c2a7ab1a72
desertTown <13631260372@163.com>
2018-05-06 20:04:44 +0800

    isFriendsWith add to User

--- src/main/java/com/evan/user/User.java
+++ src/main/java/com/evan/user/User.java
@@ -24,4 +24,8 @@ public class User {
     public List<Trip> trips() {
         return trips;
     }
+
+    public boolean isFriendsWith(User user) {
+        return friends.contains(user);
+    }
 }
--- src/test/java/com/evan/user/UserTest.java
+++ src/test/java/com/evan/user/UserTest.java
@@ -1,10 +1,27 @@
 package com.evan.user;
 
+import com.evan.trip.UserBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserTest {
-    @Test
-    public void test01() {
-        System.out.println("xx");
+    private static final User EVAN = new User();
+    private static final User BOB = new User();
+
+    @Test public void
+    should_inform_when_user_are_not_friend() {
+        User user = UserBuilder.aUser()
+                        .friendsWith(EVAN)
+                        .build();
+        Assert.assertFalse(user.isFriendsWith(BOB));
+    }
+
+    @Test public void
+    should_inform_when_user_are_friend() {
+        User user = UserBuilder.aUser()
+                .friendsWith(EVAN)
+                .friendsWith(BOB)
+                .build();
+        Assert.assertTrue(user.isFriendsWith(BOB));
     }
 }
